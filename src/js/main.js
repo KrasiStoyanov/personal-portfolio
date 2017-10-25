df00aaf8f6418ec2c878db7466f433e252b4ee38
KrasiStoyanov <krasimir641@gmail.com>
2017-10-25 17:38:25 +0200

    Made the grid layout

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+	function resizeCells (isMobile) {
+		var gridRows = $('#recent-works .row');
+		gridRows.each(function (index, row) {
+			row = $(row);
+			var rowWidth = row.outerWidth();
+			var cellsWithSpecificHeight = row.find('[data-height-cells]');
+
+			cellsWithSpecificHeight.each(function (index, cell) {
+				cell = $(cell);
+				var cellParent = cell.parent();
+				var height = cell.attr('data-height-cells');
+
+				if (isMobile) {
+					height = cell.attr('data-height-cells-mobile');
+				}
+
+				if (!cellParent.hasClass('cell-wrapper')) {
+					grid.resizeCell(cell, height, rowWidth);
+				} else {
+					grid.resizeCell(cell, height, rowWidth, true);
+				}
+			});
+		});
+	}
+
+	var windowWidth = $(window).width();
+	if (windowWidth <= 768) {
+		resizeCells(true);
+	} else {
+		resizeCells();
+	}
+
+	$(window).resize(function () {
+		var windowWidth = $(window).width();
+		if (windowWidth <= 768) {
+			resizeCells(true);
+		} else {
+			resizeCells();
+		}
+	});
+});
---
+++ src/js/modules/grid.js
@@ -0,0 +1,15 @@
+var grid = (function () {
+	var GUTTER = 30;
+	var ONE_COL_SIZE = 100 / 12;
+
+	var resizeCell = function (el, height, rowWidth, gutter) {
+		var percentageHeight = ONE_COL_SIZE * height;
+		var finalHeight = (percentageHeight * rowWidth) / 100;
+		
+		el.outerHeight(finalHeight - GUTTER);
+	}
+
+	return {
+		resizeCell: resizeCell
+	}
+}());
