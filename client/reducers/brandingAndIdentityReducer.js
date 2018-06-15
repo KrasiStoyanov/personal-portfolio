0f0cf126f7e6be309f99171d7768c8020694ca99
Aleksandar Ginovski <aleksander.ginovski@gmail.com>
2018-06-15 09:06:22 +0300

    .

--- client/reducers/brandingAndIdentityReducer.js
+++ client/reducers/brandingAndIdentityReducer.js
@@ -1,105 +1,114 @@
-export default function () {
+export default function() {
 	return [
 		{
-			"id": 0,
-			"url": "/branding-and-identity/krasi-stoyanov",
-			"title": "Krasi Stoyanov",
-			"date": "19 October, 2017",
-			"imageSrc": "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
-			"logo": "./images/branding-and-identity/krasi-stoyanov/logo.svg",
-			"category": 2,
-			"overview": {
-				"title": "Overview",
-				"heading": "The brand",
-				"description": "This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
+			id: 0,
+			url: "/branding-and-identity/krasi-stoyanov",
+			title: "Krasi Stoyanov",
+			date: "19 October, 2017",
+			imageSrc: "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
+			logo: "./images/branding-and-identity/krasi-stoyanov/logo.svg",
+			category: 2,
+			overview: {
+				title: "Overview",
+				heading: "The brand",
+				description:
+					"This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
 			},
-			"quote": {
-				"quote": ["Great acts", "are made up", "of small deeds"]
+			quote: {
+				quote: ["Great acts", "are made up", "of small deeds"],
 			},
-			"purpose": {
-				"title": "how the project was received",
-				"heading": "The purpose",
-				"description": "Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>)."
+			purpose: {
+				title: "how the project was received",
+				heading: "The purpose",
+				description:
+					"Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>).",
 			},
-			"approach": {
-				"title": "how the project was approached",
-				"heading": "The approach",
-				"description": "Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
-				"images": [
-					"./images/branding-and-identity/krasi-stoyanov/approach-1.jpg",
-					"./images/branding-and-identity/krasi-stoyanov/approach-2.jpg"
-				]
+			approach: {
+				title: "how the project was approached",
+				heading: "The approach",
+				description:
+					"Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
+				images: [
+					"./images/branding-and-identity/krasi-stoyanov/approach-1.JPG",
+					"./images/branding-and-identity/krasi-stoyanov/approach-2.JPG",
+				],
 			},
-			"images": [
+			images: [
 				"./images/branding-and-identity/krasi-stoyanov/canvas.jpg",
 				"./images/branding-and-identity/krasi-stoyanov/hats.jpg",
-				"./images/branding-and-identity/krasi-stoyanov/wall-canvas.jpg"
+				"./images/branding-and-identity/krasi-stoyanov/wall-canvas.jpg",
 			],
-			"colMd": 4
+			colMd: 4,
 		},
 		{
-			"id": 1,
-			"url": "/branding-and-identity/krasi-stoyanov-2",
-			"title": "Krasi Stoyanov 2",
-			"date": "19 October, 2017",
-			"imageSrc": "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
-			"logo": "./images/branding-and-identity/krasi-stoyanov/logo.svg",
-			"category": 2,
-			"overview": {
-				"title": "Overview",
-				"heading": "The brand",
-				"description": "This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
+			id: 1,
+			url: "/branding-and-identity/krasi-stoyanov-2",
+			title: "Krasi Stoyanov 2",
+			date: "19 October, 2017",
+			imageSrc: "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
+			logo: "./images/branding-and-identity/krasi-stoyanov/logo.svg",
+			category: 2,
+			overview: {
+				title: "Overview",
+				heading: "The brand",
+				description:
+					"This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
 			},
-			"quote": {
-				"quote": ["Great acts", "are made up", "of small deeds"]
+			quote: {
+				quote: ["Great acts", "are made up", "of small deeds"],
 			},
-			"purpose": {
-				"title": "how the project was received",
-				"heading": "The purpose",
-				"description": "Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>)."
+			purpose: {
+				title: "how the project was received",
+				heading: "The purpose",
+				description:
+					"Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>).",
 			},
-			"approach": {
-				"title": "how the project was approached",
-				"heading": "The approach",
-				"description": "Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
-				"images": [
-					"./images/branding-and-identity/krasi-stoyanov/approach-1.jpg",
-					"./images/branding-and-identity/krasi-stoyanov/approach-2.jpg"
-				]
+			approach: {
+				title: "how the project was approached",
+				heading: "The approach",
+				description:
+					"Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
+				images: [
+					"./images/branding-and-identity/krasi-stoyanov/approach-1.JPG",
+					"./images/branding-and-identity/krasi-stoyanov/approach-2.JPG",
+				],
 			},
-			"colMd": 4
+			colMd: 4,
 		},
 		{
-			"id": 2,
-			"url": "/branding-and-identity/krasi-stoyanov-3",
-			"title": "Krasi Stoyanov 3",
-			"date": "19 October, 2017",
-			"imageSrc": "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
-			"logo": "./images/branding-and-identity/krasi-stoyanov/logo.svg",
-			"category": 2,
-			"overview": {
-				"title": "Overview",
-				"heading": "The brand",
-				"description": "This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
+			id: 2,
+			url: "/branding-and-identity/krasi-stoyanov-3",
+			title: "Krasi Stoyanov 3",
+			date: "19 October, 2017",
+			imageSrc: "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
+			logo: "./images/branding-and-identity/krasi-stoyanov/logo.svg",
+			category: 2,
+			overview: {
+				title: "Overview",
+				heading: "The brand",
+				description:
+					"This is not just a monogram, this is the representation of the person using a single monogram/symbol. It demonstrates a certain amount of energy and creativity, combined into a single yet somehow, complicated digital representation.",
 			},
-			"quote": {
-				"quote": ["Great acts", "are made up", "of small deeds"]
+			quote: {
+				quote: ["Great acts", "are made up", "of small deeds"],
 			},
-			"purpose": {
-				"title": "how the project was received",
-				"heading": "The purpose",
-				"description": "Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>)."
+			purpose: {
+				title: "how the project was received",
+				heading: "The purpose",
+				description:
+					"Being a part of a Design oriented study program, a task was issued to develop a personal, inspired by everything, monogram of your initials (in this case - <span className='text-primary'>K. S.</span>).",
 			},
-			"approach": {
-				"title": "how the project was approached",
-				"heading": "The approach",
-				"description": "Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
-				"images": [
-					"./images/branding-and-identity/krasi-stoyanov/approach-1.jpg",
-					"./images/branding-and-identity/krasi-stoyanov/approach-2.jpg"
-				]
+			approach: {
+				title: "how the project was approached",
+				heading: "The approach",
+				description:
+					"Having to combine letters into a good looking logo is always considered hardship. Every monogram tells a story, that is why each monogram has its symbolic description. Each piece of it is a particular part of or a whole object, idea, symbol, etc. That is why the main symbol here is the <span className='text-primary'>leaf</span>.",
+				images: [
+					"./images/branding-and-identity/krasi-stoyanov/approach-1.JPG",
+					"./images/branding-and-identity/krasi-stoyanov/approach-2.JPG",
+				],
 			},
-			"colMd": 4
-		}
-	]
-};
+			colMd: 4,
+		},
+	];
+}
--- client/reducers/portfolioCategoriesReducer.js
+++ client/reducers/portfolioCategoriesReducer.js
@@ -1,52 +1,52 @@
-export default function () {
+export default function() {
 	return [
 		{
-			"url": "/web-design",
-			"icon": "web-design",
-			"title": "Web design",
-			"dataFilter": 1,
-			"colMd": 4,
-			"imageSrc": "./images/web-design/quizco/cover-image.png"
+			url: "/web-design",
+			icon: "web-design",
+			title: "Web design",
+			dataFilter: 1,
+			colMd: 4,
+			imageSrc: "./images/web-design/quizco/cover-image.png",
 		},
 		{
-			"url": "/branding-and-identity",
-			"icon": "branding-and-identity",
-			"title": "Branding & identity",
-			"dataFilter": 2,
-			"colMd": 4,
-			"imageSrc": "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg"
+			url: "/branding-and-identity",
+			icon: "branding-and-identity",
+			title: "Branding & identity",
+			dataFilter: 2,
+			colMd: 4,
+			imageSrc: "./images/branding-and-identity/krasi-stoyanov/cover-image.jpg",
 		},
 		{
-			"url": "/graphic-design",
-			"icon": "graphic-design",
-			"title": "graphic design",
-			"dataFilter": 3,
-			"colMd": 4,
-			"imageSrc": "./images/graphic-design/natured/cover-image.png"
+			url: "/graphic-design",
+			icon: "graphic-design",
+			title: "graphic design",
+			dataFilter: 3,
+			colMd: 4,
+			imageSrc: "./images/graphic-design/natured/cover-image.png",
 		},
 		{
-			"url": "/art-and-illustration",
-			"icon": "art-and-illustration",
-			"title": "Art & illustration",
-			"dataFilter": 4,
-			"colMd": 4,
-			"imageSrc": "./images/art-and-illustration/space-cara.png"
+			url: "/art-and-illustration",
+			icon: "art-and-illustration",
+			title: "Art & illustration",
+			dataFilter: 4,
+			colMd: 4,
+			imageSrc: "./images/art-and-illustration/space-cara.png",
 		},
 		{
-			"url": "/literature",
-			"icon": "literature",
-			"title": "Literature",
-			"dataFilter": 5,
-			"colMd": 4,
-			"imageSrc": "./images/literature/art-of-loneliness.jpeg"
+			url: "/literature",
+			icon: "literature",
+			title: "Literature",
+			dataFilter: 5,
+			colMd: 4,
+			imageSrc: "./images/literature/art-of-loneliness.jpeg",
 		},
 		{
-			"url": "/photography",
-			"icon": "photography",
-			"title": "Photography",
-			"dataFilter": 6,
-			"colMd": 4,
-			"imageSrc": "./images/photography/blossom.jpg"
-		}
-	]
-};
+			url: "/photography",
+			icon: "photography",
+			title: "Photography",
+			dataFilter: 6,
+			colMd: 4,
+			imageSrc: "./images/photography/blossom.JPG",
+		},
+	];
+}
