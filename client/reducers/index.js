1cd902806294ec07323a3254124565bf6ebe1342
Aleksandar Ginovski <aleksander.ginovski@gmail.com>
2018-06-15 08:12:57 +0300

    .

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,15 +1,15 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import artAndIllustration from './artAndIllustrationReducer';
-import brandingAndIdentity from './brandingAndIdentityReducer';
-import graphicDesign from './graphicDesignReducer';
-import literature from './literatureReducer';
-import photography from './photographyReducer';
-import webDesign from './webDesignReducer';
+import artAndIllustration from "./artAndIllustrationReducer";
+import brandingAndIdentity from "./brandingAndIdentityReducer";
+import graphicDesign from "./graphicDesignReducer";
+import literature from "./literatureReducer";
+import photography from "./photographyReducer";
+import webDesign from "./webDesignReducer";
 
-import navigationBarColorSchemeClass from './navigationBarColorSchemeClass';
-import navigationLinks from './navigationLinksReducer';
-import portfolioCategories from './portfolioCategoriesReducer';
+import navigationBarColorSchemeClass from "./navigationBarColorSchemeClass";
+import navigationLinks from "./NavigationLinksReducer";
+import portfolioCategories from "./portfolioCategoriesReducer";
 
 const allReducers = combineReducers({
 	artAndIllustration,
