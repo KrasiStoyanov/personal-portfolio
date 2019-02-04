31a0c3731de26727d16de7ee11eab18ee71c0a9b
Krasimir Stoyanov <krasimir641@gmail.com>
2019-02-04 23:56:23 +0100

    Changed about me text

--- client/pages/About.js
+++ client/pages/About.js
@@ -18,7 +18,7 @@ class About extends Component {
 		    			<section className="section col-12 col-lg-6 pl-5">
                             <small className="title text-uppercase text-primary mb-1">Krasi Stoyanov</small>
 		    				<h2 className="color-black mb-3 text-capitalize">UI Designer and Front End Developer</h2>
-		    				<p className="mb-0">A 19-year-old prodigy, combining design and front-end skills, volunteering in different causes and a great team-player, who will never leave you on your own. Extremely diligent, life-time learner, with the intention to become a front-end grand master.
+		    				<p className="mb-0">A 20-year-old prodigy, combining design and front-end skills, volunteering in different causes and a great team-player, who will never leave you on your own. Extremely diligent, life-time learner, with the intention to become a front-end grand master.
 		    				<br/>
 		    				<br/>
 		    				Currently a 2nd year Game Design & Development student <a href="https://www.hanze.nl/nld" target="_blank">@Hanzehogeschool</a> Groningen.</p>
